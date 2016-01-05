e3a75090984a37b7f68a336719b67bf05bb41d1a
Simon323 <szmiekus@gmail.com>
2016-01-05 21:44:40 +0100

    Base access to data

---
+++ MMDS.Model/Repository/OfferRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMDS.Model.Repository
+{
+    public class OfferRepository// : BaseDbContextRepository<, AllegroEntities>, IOfferRepository
+    {
+    }
+}
