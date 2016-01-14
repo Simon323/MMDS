82e368bf11da7214ab011c93db5fe3123443f550
Simon323 <szmiekus@gmail.com>
2016-01-14 02:21:45 +0100

    @lab-10 find similar user

---
+++ MMDS.Lab/CSVProcessing.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMDS.Lab
+{
+    public class CSVProcessing
+    {
+        public static Dictionary<int, List<int>> ReadAllFactsToMemory()
+        {
+            bool firstRow = true;
+            Dictionary<int, List<int>> usersDictionary = new Dictionary<int, List<int>>();
+            var watch = Stopwatch.StartNew();
+            using (FileStream fs = File.Open(@"D:\Studia\MMDS\msdc-facts\facts.csv", FileMode.Open))
+            using (BufferedStream bs = new BufferedStream(fs))
+            using (StreamReader sr = new StreamReader(bs))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    if (firstRow)
+                    {
+                        firstRow = false;
+                        continue;
+                    }
+
+                    var values = s.Split(',');
+                    int user = int.Parse(values[1].Trim());
+                    int song = int.Parse(values[0].Trim());
+
+                    if (!usersDictionary.ContainsKey(user))
+                    {
+                        var songList = new List<int>();
+                        songList.Add(song);
+                        usersDictionary.Add(user, songList);
+                    }
+                    else
+                    {
+                        var dictionaryItem = usersDictionary[user];
+
+                        if (!dictionaryItem.Contains(song))
+                            dictionaryItem.Add(song);
+                    }
+                        
+
+                    //factsList.Add(new Facts(int.Parse(values[0].Trim()), int.Parse(values[1].Trim()), int.Parse(values[2].Trim())));
+
+                }
+            }
+
+            watch.Stop();
+            Console.Clear();
+            Console.WriteLine("Time elapsed: {0:hh\\:mm\\:ss}", watch.Elapsed);
+
+            return usersDictionary;
+        }
+    }
+}
---
+++ MMDS.Lab/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMDS.Lab
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, List<int>> factsDictionary = CSVProcessing.ReadAllFactsToMemory();
+            var usersDictionary = factsDictionary.Take(100);
+            Dictionary<int, List<string>> result = new Dictionary<int, List<string>>();
+            int iterator = 0;
+            var watch = Stopwatch.StartNew();
+
+            foreach (var user in usersDictionary)
+            {
+                Dictionary<int, double> temp = new Dictionary<int, double>();
+                foreach (var fact in factsDictionary)
+                {
+                    if (user.Key.Equals(fact.Key))
+                        continue;
+
+                    double intersectCount = user.Value.Select(x => x).Intersect(fact.Value).Count();
+                    double unionCounter = user.Value.Union(fact.Value).ToList().Count();
+                    double divideResult = unionCounter.Equals(0) ? 0 : (intersectCount / unionCounter);
+
+                    temp.Add(fact.Key, divideResult);
+                }
+
+                List<string> res = new List<string>();
+                foreach (var x in temp.OrderByDescending(x => x.Value).Take(100))
+                {
+                    res.Add(x.Key + " " + x.Value);
+                }
+
+                result.Add(user.Key, res);
+                iterator++;
+            }
+
+            watch.Stop();
+            Console.WriteLine("Time elapsed: {0:hh\\:mm\\:ss}", watch.Elapsed);
+
+            Console.ReadLine();
+        }
+    }
+}
